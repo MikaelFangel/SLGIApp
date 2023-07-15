dd5daf0629010220f93edb2a94020b209b78918e
Philip Astrup Cramer <s224319@dtu.dk>
2023-07-15 17:31:26 +0200

    created first edition top bar

---
+++ app/src/main/java/com/slgi/slgiapp/ui/shared/TopBar.kt
@@ -0,0 +1,38 @@
+package com.slgi.slgiapp.ui.shared
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.slgi.slgiapp.ui.theme.SLGIAppTheme
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopBar(barTitle: String){
+    TopAppBar(title = {
+        Text(text = barTitle)
+    },
+        navigationIcon = {
+            Icon(
+                Icons.Default.KeyboardArrowLeft,
+                contentDescription = null
+            )}
+    )
+}
+
+
+@Preview (showBackground = true)
+@Composable
+fun TopBarPreview(){
+    SLGIAppTheme(dynamicColor = false){
+        Surface() {
+            TopBar("Profil Oplysninger")
+        }
+    }
+}
