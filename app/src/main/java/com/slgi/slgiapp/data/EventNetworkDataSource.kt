dac24dd7723e06ca4bb7cc9338fda6c2f85c2db7
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-08-15 21:21:53 +0200

    refactored event network data source

--- app/src/main/java/com/slgi/slgiapp/data/EventNetworkDataSource.kt
+++ app/src/main/java/com/slgi/slgiapp/data/EventNetworkDataSource.kt
@@ -2,6 +2,7 @@ package com.slgi.slgiapp.data
 
 import com.google.firebase.Timestamp
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.ktx.dataObjects
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -9,44 +10,40 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.tasks.await
 
 class EventNetworkDataSource : EventDateSourceApi {
-    override val events: Flow<List<Event>>
+    private val eventCollection: CollectionReference
         get() = Firebase.firestore
             .collection(EVENT_COLLECTION)
+
+    override val events: Flow<List<Event>>
+        get() = eventCollection
             .orderBy(DATE_FIELD)
             .whereGreaterThanOrEqualTo(DATE_FIELD, Timestamp.now())
             .dataObjects()
 
-    override suspend fun toggleParticipation(id: String) {
-        val userId = Firebase.auth.uid?: return
+    override suspend fun toggleParticipation(eventId: String) {
+        val userId = Firebase.auth.uid ?: return
 
-        val participationCollection = Firebase.firestore
-            .collection(EVENT_COLLECTION)
-            .document(id)
-            .collection(PARTICIPATION_COLLECTION)
-
-        if (!isParticipating(id)) {
-            participationCollection.add(User(userId = userId)).await()
+        if (!isParticipating(eventId)) {
+            participantCollection(eventId).add(User(userId = userId)).await()
         } else {
-            participationCollection.whereEqualTo(USERID_FIELD, userId)
+            participantCollection(eventId).whereEqualTo(USERID_FIELD, userId)
                 .get().await().documents.forEach { d ->
-                    participationCollection.document(d.id).delete()
+                    participantCollection(eventId).document(d.id).delete()
                 }
         }
     }
 
-    override suspend fun isParticipating(id: String): Boolean {
-        val userId = Firebase.auth.uid?: return false
-
-        val participationCollection = Firebase.firestore
-            .collection(EVENT_COLLECTION)
-            .document(id)
-            .collection(PARTICIPATION_COLLECTION)
+    override suspend fun isParticipating(eventId: String): Boolean {
+        val userId = Firebase.auth.uid ?: return false
 
         // Check if user is registered or not
-        return participationCollection
+        return participantCollection(eventId)
             .whereEqualTo(USERID_FIELD, userId)
             .get().await().documents.isNotEmpty()
     }
+
+    private fun participantCollection(eventId: String): CollectionReference =
+        eventCollection.document(eventId).collection(PARTICIPATION_COLLECTION)
 
     companion object {
         private const val EVENT_COLLECTION = "Events"
