6df121178770d7fb41ac0dcbe183190a29218416
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-10-09 14:32:57 +0200

    added keyboard options

--- app/src/main/java/com/slgi/slgiapp/ui/RegistrationScreen.kt
+++ app/src/main/java/com/slgi/slgiapp/ui/RegistrationScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -14,15 +15,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.slgi.slgiapp.R
 import com.slgi.slgiapp.ui.shared.TopBar
 
 @Composable
 fun RegistrationScreen() {
-    Scaffold (
+    Scaffold(
         topBar = { TopBar(barTitle = stringResource(id = R.string.requestAccessLabel)) }
-    ) {innerPadding ->
+    ) { innerPadding ->
         Column(
             Modifier
                 .fillMaxSize()
@@ -31,19 +34,42 @@ fun RegistrationScreen() {
         ) {
             OutlinedTextField(
                 label = { Text(text = stringResource(id = R.string.firstNameLabel)) },
-                value = "", onValueChange = {})
+                value = "", onValueChange = {},
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                )
+            )
             OutlinedTextField(
                 label = { Text(text = stringResource(id = R.string.lastNameLabel)) },
-                value = "", onValueChange = {})
+                value = "", onValueChange = {},
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Next
+                )
+            )
             OutlinedTextField(
                 label = { Text(text = stringResource(id = R.string.emailLabel)) },
-                value = "", onValueChange = {})
+                value = "", onValueChange = {},
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Email,
+                    imeAction = ImeAction.Next
+                )
+            )
             OutlinedTextField(
                 label = { Text(text = stringResource(id = R.string.passwordLable)) },
-                value = "", onValueChange = {})
+                value = "", onValueChange = {},
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Next
+                )
+            )
             OutlinedTextField(
                 label = { Text(text = stringResource(id = R.string.repeatPasswordLabel)) },
-                value = "", onValueChange = {})
+                value = "", onValueChange = {},
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                )
+            )
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Switch(checked = false, onCheckedChange = {})
                 Spacer(modifier = Modifier.padding(5.dp))
