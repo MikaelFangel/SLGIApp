6d20b23d365ec02292eff4ebef1dfa0698836fc9
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-10-09 14:37:24 +0200

    added keyboard actions

--- app/src/main/java/com/slgi/slgiapp/ui/RegistrationScreen.kt
+++ app/src/main/java/com/slgi/slgiapp/ui/RegistrationScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -14,6 +15,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -23,6 +26,8 @@ import com.slgi.slgiapp.ui.shared.TopBar
 
 @Composable
 fun RegistrationScreen() {
+    val focusManager = LocalFocusManager.current
+
     Scaffold(
         topBar = { TopBar(barTitle = stringResource(id = R.string.requestAccessLabel)) }
     ) { innerPadding ->
@@ -37,6 +42,9 @@ fun RegistrationScreen() {
                 value = "", onValueChange = {},
                 keyboardOptions = KeyboardOptions(
                     imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
                 )
             )
             OutlinedTextField(
@@ -44,6 +52,9 @@ fun RegistrationScreen() {
                 value = "", onValueChange = {},
                 keyboardOptions = KeyboardOptions(
                     imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
                 )
             )
             OutlinedTextField(
@@ -52,6 +63,9 @@ fun RegistrationScreen() {
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Email,
                     imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
                 )
             )
             OutlinedTextField(
@@ -60,6 +74,9 @@ fun RegistrationScreen() {
                 keyboardOptions = KeyboardOptions(
                     keyboardType = KeyboardType.Password,
                     imeAction = ImeAction.Next
+                ),
+                keyboardActions = KeyboardActions(
+                    onNext = { focusManager.moveFocus(FocusDirection.Down) }
                 )
             )
             OutlinedTextField(
