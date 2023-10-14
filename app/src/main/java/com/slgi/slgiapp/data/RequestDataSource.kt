7d26a92c3c9744bee849f7c5c5386db9d7992771
Philip Cramer <s224319@dtu.dk>
2023-10-14 23:27:58 +0200

    implemented getPendingRequest and delete request functions

--- app/src/main/java/com/slgi/slgiapp/data/RequestDataSource.kt
+++ app/src/main/java/com/slgi/slgiapp/data/RequestDataSource.kt
@@ -1,10 +1,20 @@
 package com.slgi.slgiapp.data
 
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.dataObjects
+import kotlinx.coroutines.flow.Flow
 
 class RequestDataSource : RequestDataSourceApi {
     override suspend fun requestAccess(request: Request) {
         FirebaseFirestore.getInstance().collection(REQUEST_COLLECTION).add(request)
+    }
+
+    override fun getPendingRequests(): Flow<List<Request>> {
+        return (FirebaseFirestore.getInstance().collection((REQUEST_COLLECTION)).dataObjects())
+    }
+
+    override suspend fun deleteRequest(request: Request) {
+        (FirebaseFirestore.getInstance().collection(REQUEST_COLLECTION)).document(request.id).delete()
     }
 
     companion object {
--- app/src/main/java/com/slgi/slgiapp/data/RequestRepository.kt
+++ app/src/main/java/com/slgi/slgiapp/data/RequestRepository.kt
@@ -1,5 +1,15 @@
 package com.slgi.slgiapp.data
 
+import kotlinx.coroutines.flow.Flow
+
 class RequestRepository(private val requestDataSource: RequestDataSourceApi) : RequestDataSourceApi {
     override suspend fun requestAccess(request: Request) = requestDataSource.requestAccess(request)
+
+    override fun getPendingRequests(): Flow<List<Request>> {
+        return requestDataSource.getPendingRequests()
+    }
+
+    override suspend fun deleteRequest(request: Request) {
+        requestDataSource.deleteRequest(request)
+    }
 }
