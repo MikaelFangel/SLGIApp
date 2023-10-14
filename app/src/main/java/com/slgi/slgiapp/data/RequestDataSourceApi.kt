aecadb1f627f86e5cf7748220946520054cc4eac
Philip Cramer <s224319@dtu.dk>
2023-10-14 23:27:40 +0200

    created getPendingRequest and delete request functions

--- app/src/main/java/com/slgi/slgiapp/data/RequestDataSourceApi.kt
+++ app/src/main/java/com/slgi/slgiapp/data/RequestDataSourceApi.kt
@@ -1,5 +1,10 @@
 package com.slgi.slgiapp.data
 
+import kotlinx.coroutines.flow.Flow
+
 interface RequestDataSourceApi {
     suspend fun requestAccess(request: Request)
+
+    fun getPendingRequests(): Flow<List<Request>>
+    suspend fun deleteRequest(request: Request)
 }
