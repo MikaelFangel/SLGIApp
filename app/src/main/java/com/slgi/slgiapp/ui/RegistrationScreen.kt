6e2644ec1d780aa8f405749ad326080e64213e1b
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-10-15 15:09:17 +0200

    made regscreen scrollable

--- app/src/main/java/com/slgi/slgiapp/ui/RegistrationScreen.kt
+++ app/src/main/java/com/slgi/slgiapp/ui/RegistrationScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -52,7 +54,8 @@ fun RegistrationScreen(viewModel: RegistrationScreenViewModel, onRequest: () -> 
         Column(
             Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             OutlinedTextField(
