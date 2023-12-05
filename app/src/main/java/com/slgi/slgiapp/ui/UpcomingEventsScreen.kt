6ba02daf069b0287ea18b4557fb2803ecb978479
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-12-05 20:52:32 +0000

    feat: indicate when flows are empty (#50)
    * feat: indicate when flows are empty

--- app/src/main/java/com/slgi/slgiapp/ui/UpcomingEventsScreen.kt
+++ app/src/main/java/com/slgi/slgiapp/ui/UpcomingEventsScreen.kt
@@ -1,20 +1,28 @@
 package com.slgi.slgiapp.ui
 
 import android.icu.text.SimpleDateFormat
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.EventBusy
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.slgi.slgiapp.R
 import com.slgi.slgiapp.ui.shared.CreateEventModal
@@ -48,35 +56,53 @@ fun UpcomingEventsScreen(
             }
         }
     ) { innerPadding ->
-        LazyColumn(Modifier.padding(innerPadding)) {
-            items(items = events.value, key = { it.id }) {
-                val userParticipation =
-                    viewModel.getParticipantFlow(it.id)?.collectAsState(initial = emptyList())
-                val participants =
-                    viewModel.getParticipants(it.id).collectAsState(initial = emptyList())
-                EventCard(
-                    eventImageURL = it.imageUrl,
-                    eventName = it.name,
-                    eventDescription = it.description,
-                    eventDate = (SimpleDateFormat(
-                        "dd-MM-yyyy",
-                        Locale.getDefault()
-                    ).format(it.dateAndTime.toDate())).toString(),
-                    eventTime = (SimpleDateFormat(
-                        "HH:mm",
-                        Locale.getDefault()
-                    ).format(it.dateAndTime.toDate().time)).toString(),
-                    eventNumberOfParticipants = participants.value.size.toString(),
-                    eventFireleader = it.fireleader,
-                    buttonText = if (userParticipation != null && userParticipation.value.isEmpty())
-                        stringResource(R.string.participate) else stringResource(R.string.resign),
-                    participating = (userParticipation != null && userParticipation.value.isEmpty()),
-                    isAdmin = loginState.value.isAdmin,
-                    onDeleteAction = { viewModel.deleteEvent(it.id) }
-                ) {
-                    CoroutineScope(Dispatchers.IO).launch {
-                        viewModel.toggleParticipation(it.id)
+        if (events.value.isNotEmpty()) {
+            LazyColumn(Modifier.padding(innerPadding)) {
+                items(items = events.value, key = { it.id }) {
+                    val userParticipation =
+                        viewModel.getParticipantFlow(it.id)?.collectAsState(initial = emptyList())
+                    val participants =
+                        viewModel.getParticipants(it.id).collectAsState(initial = emptyList())
+                    EventCard(
+                        eventImageURL = it.imageUrl,
+                        eventName = it.name,
+                        eventDescription = it.description,
+                        eventDate = (SimpleDateFormat(
+                            "dd-MM-yyyy",
+                            Locale.getDefault()
+                        ).format(it.dateAndTime.toDate())).toString(),
+                        eventTime = (SimpleDateFormat(
+                            "HH:mm",
+                            Locale.getDefault()
+                        ).format(it.dateAndTime.toDate().time)).toString(),
+                        eventNumberOfParticipants = participants.value.size.toString(),
+                        eventFireleader = it.fireleader,
+                        buttonText = if (userParticipation != null && userParticipation.value.isEmpty())
+                            stringResource(R.string.participate) else stringResource(R.string.resign),
+                        participating = (userParticipation != null && userParticipation.value.isEmpty()),
+                        isAdmin = loginState.value.isAdmin,
+                        onDeleteAction = { viewModel.deleteEvent(it.id) }
+                    ) {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            viewModel.toggleParticipation(it.id)
+                        }
                     }
+                }
+            }
+        } else {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Icon(
+                        Icons.Filled.EventBusy,
+                        contentDescription = null,
+                        modifier = Modifier.size(50.dp)
+                    )
+                    Text(text = stringResource(id = R.string.noNewEvents))
                 }
             }
         }
