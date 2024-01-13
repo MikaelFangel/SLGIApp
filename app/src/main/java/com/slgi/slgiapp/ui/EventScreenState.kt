018836b3818f0280e1286fb4cfe278081af95838
Philip Cramer <s224319@dtu.dk>
2024-01-14 00:57:54 +0100

    changed initial value of newEventDate to be start of the day to avoid time errors

--- app/src/main/java/com/slgi/slgiapp/ui/EventScreenState.kt
+++ app/src/main/java/com/slgi/slgiapp/ui/EventScreenState.kt
@@ -1,7 +1,9 @@
 package com.slgi.slgiapp.ui
 
 import com.google.firebase.Timestamp
+import java.time.LocalDate
 import java.time.LocalTime
+import java.time.ZoneOffset
 
 data class EventScreenState(
     val displayCreateDialog: Boolean = false,
@@ -10,7 +12,7 @@ data class EventScreenState(
     val newEventName: String = "",
     val newEventDescription: String = "",
     val newEventTime: LocalTime = LocalTime.of(LocalTime.now().hour.plus(1).mod(24),0,0),
-    val newEventDate: Timestamp = Timestamp.now(),
+    val newEventDate: Timestamp = Timestamp(LocalDate.now().atStartOfDay().toEpochSecond(ZoneOffset.UTC), 0),
     val newEventImageURL: String = "",
     val newEventFireLeader: String = ""
 )
